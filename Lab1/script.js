beb163fb2e3e63f6cca1abcce662f574a755c8d4
bsaroya <balrajssaroya@gmail.com>
2025-01-17 20:39:57 -0500

    initial commit
    initial commit - Lab 1

---
+++ Lab1/script.js
@@ -0,0 +1,38 @@
+let studentName = "Balraj Saroya";
+let studentAge = 19;
+let isEnrolled = true;
+
+console.log("Student Name:", studentName);
+console.log("Student Age:", studentAge);
+console.log("Is Enrolled:", isEnrolled);
+
+
+let text = "Hello, JavaScript!";
+console.log("String Example:", text);
+
+let number = 42;
+console.log("Number Example:", number);
+
+let isLearning = true;
+console.log("Boolean Example:", isLearning);
+
+let fruits = ["peach", "banana", "grape"];
+console.log("Array Example:", fruits);
+
+let person = { firstName: "Jane", lastName: "Doe" };
+console.log("Object Example:", person);
+
+let a = 10, b = 5;
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+
+console.log("Greater than:", a > b);
+console.log("Less than:", a < b);
+console.log("Equal to:", a === b);
+console.log("Not equal:", a !== b);
+
+console.log("AND (true && false):", true && false);
+console.log("OR (true || false):", true || false);
+console.log("NOT (!true):", !true);
